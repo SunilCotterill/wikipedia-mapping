48cc95183b91e9e3c0c4a66add41f2371f592594
SunilCotterill <sunilcotterill@gmail.com>
2021-12-14 23:29:32 -0500

    Added coordinate calculations

--- wikipedia_scraper.py
+++ wikipedia_scraper.py
@@ -2,6 +2,9 @@ import os, sys, argparse, datetime, atexit, urllib.request
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from shapely.geometry import Point
+import geopandas as gpd
+from geopandas import GeoDataFrame
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
@@ -16,8 +19,36 @@ tables = page_soup.find_all(id='coordinates')
 refs = tables[0].find_all('a', {'class': 'external text'})
 link = refs[0].attrs.get('href')
 lhs,rhs = link.split('params=')
+
+#this won't work because they don't always have 19 chars
 coordinates = rhs[:20]
 print(coordinates)
+
+
+def coordinateToNum(coordinate):
+    coordinate1 = coordinate[:10].replace('_','.',1).replace('_','')
+    coordinate2 = coordinate[10:].replace('_','.',1).replace('_','')
+    if 'N' in coordinate1:
+        coordinate1 = float(coordinate1.replace('N', ''))
+    else:
+        coordinate1 = -1*float(coordinate1.replace('S', '')) 
+
+    if 'E' in coordinate2:
+        coordinate2 = float(coordinate2.replace('E', ''))
+    else:
+        coordinate2 = -1*float(coordinate2.replace('W', '')) 
+
+    
+    print(coordinate1)
+    print(coordinate2)
+    return coordinate1, coordinate2
+
+
+numCoor1, numCoor2 = coordinateToNum(coordinates)
+
+plt.scatter(x=numCoor1, y=numCoor2)
+plt.show()
+
 
 
 
